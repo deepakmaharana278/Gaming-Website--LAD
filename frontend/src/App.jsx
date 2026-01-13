065378657a34c2b3a2d7f050aa9f546cd1e7d57c
M Tarini prasad <kashinathmaharana196@gmail.com>
2026-01-13 08:53:56 +0530

    Games fetched...

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,6 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./Auth/Login";
 import Register from "./Auth/Register";
-<<<<<<< Updated upstream
-
-=======
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Privacy from "./pages/Privacy";
@@ -13,16 +10,13 @@ import TermsConditions from "./pages/TermsConditions";
 import Disclaimer from "./pages/Disclaimer";
 import Games from "./pages/Games";
 import GamePlayer from "./pages/GamePlayer";
->>>>>>> Stashed changes
 
 function App() {
   return (
     <Routes>
-<<<<<<< Updated upstream
       <Route path="/" element={<Home/>} />
       <Route path="/login" element={<Login/>} />
       <Route path="/signup" element={<Register/>} />
-=======
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Register />} />
@@ -35,7 +29,6 @@ function App() {
       <Route path="/game/:id" element={<GamePlayer />} />
       <Route path="/all-games" element={<Games />} />
       <Route path="/trending" element={<Games />} />
->>>>>>> Stashed changes
     </Routes>
   );
 }
