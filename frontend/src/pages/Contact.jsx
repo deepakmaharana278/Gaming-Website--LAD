15eddbd374c98b5ff675a6be6dc33f0523588b85
Jitendra Maharana <deepakmaharana3500@gmail.com>
2026-01-14 22:48:46 +0530

    Fix EmailJS package for Vercel

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Layout from "../components/Layout";
-import emailjs from "emailjs-com";
+import emailjs from "@emailjs/browser";
 import { ToastContainer, toast } from 'react-toastify'
 import "react-toastify/dist/ReactToastify.css"
 
