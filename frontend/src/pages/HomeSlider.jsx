fa52d823776658f4a2ba37619975c6f80a424ce5
Jitendra Maharana <deepakmaharana3500@gmail.com>
2026-01-28 20:08:28 +0530

    replace jpg image to webp for better performance

--- frontend/src/pages/HomeSlider.jsx
+++ frontend/src/pages/HomeSlider.jsx
@@ -3,10 +3,10 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
-import slide1 from "../assets/slide1.jpg";
-import slide2 from "../assets/slide2.jpg";
-import slide3 from "../assets/slide3.jpg";
-import slide4 from "../assets/slide4.jpg";
+import slide1 from "../assets/slide1.webp";
+import slide2 from "../assets/slide2.webp";
+import slide3 from "../assets/slide3.webp";
+import slide4 from "../assets/slide4.webp";
 import { useNavigate } from "react-router-dom";
 
 const HomeSlider = () => {
